a91f851c39a10ca7f47f91605e64194d7afd8fe8
Nagireddy Chereddy <75054799+nagireddy00@users.noreply.github.com>
2024-10-03 15:02:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "jenkins-vpc"
-  cidr =  var.vpc_cidr_blocks.id
+  cidr = var.vpc_cidr_blocks.id
 
 
-  azs             = data.aws_availability_zones.azs.names
-  public_subnets  = var.public_subnets_cidr_blocks
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets_cidr_blocks
 
-  enable_dns_hostnames = true
+  enable_dns_hostnames    = true
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "jenkins-vpc"
-    Terraform = "true"
+    Name        = "jenkins-vpc"
+    Terraform   = "true"
     Environment = "dev"
   }
 
@@ -69,12 +69,96 @@ module "ec2_instance" {
   name = "Jenkins-Server"
 
   instance_type               = var.instance_type
-  key_name                    = "jenkins-server-key"
+  key_name                    = ""
   monitoring                  = true
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data                   = file("jenkins-install.sh")
+  user_data                   = file("D:/MyWorkspace/mynotebooks/first-project/infra/Eks-cluster/install.sh")
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+
+  tags = {
+    Name        = "Jenkins-Server"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}#creating vpc for my jenkins server 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr_blocks.id
+
+
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets_cidr_blocks
+
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "jenkins-vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
+#module security group
+module "sg" {
+
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-sg"
+  description = "Security Group for Jenkins Server"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+# EC2
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Jenkins-Server"
+
+  instance_type               = var.instance_type
+  key_name                    = "D:/MyWorkspace/mynotebooks/first-project/mykeypair.pem"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  user_data                   = file("D:/MyWorkspace/mynotebooks/first-project/infra/Eks-cluster/install.sh")
   availability_zone           = data.aws_availability_zones.azs.names[0]
 
   tags = {
